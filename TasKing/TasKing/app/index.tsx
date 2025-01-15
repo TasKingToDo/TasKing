32c4ea3dd70dca1223b37f4aefcbfe14faacec9b
Bryce Bailey <brycebailey287@gmail.com>
2025-01-14 23:05:15 -0600

    Added files, moved files around, begun workings of login/signup page

---
+++ TasKing/TasKing/app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import LoginScreen from "./screens/OpeningScreen";
+import OpeningScreen from "./screens/OpeningScreen";
+
+export default function Index() {
+  return (
+    <OpeningScreen />
+  );
+}
