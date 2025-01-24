cf22ba3e9eec8e7e2210b4e81815c931f0ca9bd2
Zoe B <zjb008@latech.edu>
2025-01-24 15:16:23 -0600

    added safety wrappers, added shop screen to nav, started on shop screen shell

--- app/screens/ShopScreen.tsx
+++ app/screens/ShopScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
-
+import { View, ScrollView, Image, Text, TextInput, Dimensions, StyleSheet } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 const ShopScreen = () => {
     return (
-        <Text> Shop Screen</Text>
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <View style={styles.background}>
+                    <Text>Shop Screen top</Text>
+                </View>
+                <ScrollView>
+                    <Text>Shop Screen bottom</Text>
+                </ScrollView>
+            </SafeAreaView>
+        </SafeAreaProvider>
+        
     );
 };
+
+const styles = StyleSheet.create({
+    background: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height / 2,
+    },
+})
 
 export default ShopScreen;
