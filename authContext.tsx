f6537bb0e82068d46485c189d9f6e901e47d87a9
JMitchell919 <jmitchell91902@gmail.com>
2025-02-23 12:57:20 -0600

    context file for authentication

---
+++ authContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from './firebaseConfig';
+import { View, ActivityIndicator } from 'react-native';
+
+export const authContext = createContext({ user: null });
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return <authContext.Provider value={{ user }}>{children}</authContext.Provider>;
+};
